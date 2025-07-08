2495e942e3514016f8c78b18e2b545ec6bcf2758
TanerJackson <jacksontaner@gmail.com>
2025-07-08 12:39:06 -0500

    seed file created pending database test

--- db/seed.js
+++ db/seed.js
@@ -1,11 +1,37 @@
 import db from "#db/client";
 import { createUser } from "#db/queries/users";
-
+import { faker } from "@faker-js/faker";
 await db.connect();
 await seed();
 await db.end();
 console.log("🌱 Database seeded.");
 
 async function seed() {
-  await createUser("foo", "bar");
+  await createUser(
+    faker.person.fullname,
+    faker.internet.username,
+    "password",
+    "org",
+    "5551112222"
+  );
+
+  for (let i = 1; i < 4; i++) {
+    await createUser(
+      faker.person.fullname,
+      faker.internet.username,
+      "password",
+      "man",
+      "5551112222"
+    );
+  }
+
+  for (let i = 1; i < 10; i++) {
+    await createUser(
+      faker.person.fullname,
+      faker.internet.username,
+      "password",
+      "sub",
+      "5551112222"
+    );
+  }
 }
