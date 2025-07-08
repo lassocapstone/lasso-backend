3f43272c868a0ab652d6c86b0e8eeb75399a38f9
Bryan Cotto <brycotto1@yahoo.com>
2025-07-08 13:57:44 -0400

    Fixed role to be accountType

--- middleware/requireManager.js
+++ middleware/requireManager.js
@@ -1,8 +1,8 @@
-export default requireManager = async (err, req, res, next) => {
-  const { role } = req.user;
+export default requireManager = async (req, res, next) => {
+  const { accountType } = req.user;
 
   //if role is not a manager
-  if(role !== "man") res.status(403).send("Authorization denied. User not a manager.");
+  if(accountType !== "man") res.status(403).send("Authorization denied. User not a manager.");
 
   next();
 }
--- middleware/requireOrganizer.js
+++ middleware/requireOrganizer.js
@@ -1,8 +1,8 @@
-export default requireOrganizer = async (err, req, res, next) => {
-  const { role } = req.user;
+export default requireOrganizer = async (req, res, next) => {
+  const { accountType } = req.user;
 
   //if role is not an organizer
-  if(role !== "org") res.status(403).send("Authorization denied. User not an organizer.");
+  if(accountType !== "org") res.status(403).send("Authorization denied. User not an organizer.");
 
   next();
 }
