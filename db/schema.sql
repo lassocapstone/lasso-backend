bbf729dca4c183800f52874b51b7a21fae3a372d
Samuel Kim <samjkim455@gmail.com>
2025-07-08 13:43:19 -0400

    schema complete

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,70 @@
+DROP TABLE IF EXISTS tasks;
+DROP TABLE IF EXISTS alerts;
+DROP TABLE IF EXISTS managers_events;
+DROP TABLE IF EXISTS subordinates_events;
+DROP TABLE IF EXISTS events;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS emergency_contact;
+ 
+CREATE TABLE emergency_contact (
+  id SERIAL PRIMARY KEY,
+  contact_number CHAR(10) NOT NULL,
+  name VARCHAR(30) NOT NULL,
+  relationship VARCHAR(30) NOT NULL
+);
 
 CREATE TABLE users (
-  id serial PRIMARY KEY,
-  username text NOT NULL UNIQUE,
-  password text NOT NULL
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(30) NOT NULL UNIQUE,
+  password VARCHAR(30) NOT NULL,
+  account_type CHAR(3) NOT NULL,
+  contact_number CHAR(10) NOT NULL,
+  role VARCHAR(30),
+  status BOOLEAN,
+  emergency_contact_id INTEGER REFERENCES emergency_contact(id) ON DELETE CASCADE
 );
+
+CREATE TABLE events (
+  id SERIAL PRIMARY KEY,
+  organizer_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE subordinates_events (
+  id SERIAL PRIMARY KEY,
+  subordinate_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  event_id INTEGER NOT NULL REFERENCES events(id) ON DELETE CASCADE,
+  manager_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE managers_events (
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  event_id INTEGER NOT NULL REFERENCES events(id) ON DELETE CASCADE
+);
+
+CREATE TABLE alerts (
+  id SERIAL PRIMARY KEY,
+  is_okay BOOLEAN NOT NULL,
+  name VARCHAR(30) NOT NULL,
+  message TEXT NOT NULL,
+  event_id INTEGER NOT NULL REFERENCES events(id) ON DELETE CASCADE,
+  organizer_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  sender_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE tasks (
+  id SERIAL PRIMARY KEY,
+  event_id INTEGER NOT NULL REFERENCES events(id) ON DELETE CASCADE,
+  name VARCHAR(30) NOT NULL,
+  start_time TIME NOT NULL,
+  end_time TIME NOT NULL,
+  location VARCHAR(30) NOT NULL,
+  instructions TEXT
+);
+
+
+
+
+
+
+
