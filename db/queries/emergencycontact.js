c7367e986cc0cd137d2c08ed667e2c7c1d28cc22
TanerJackson <jacksontaner@gmail.com>
2025-07-09 11:08:00 -0500

    emergency contact query created

---
+++ db/queries/emergencycontact.js
@@ -0,0 +1,16 @@
+import db from "#db/client";
+
+export async function createEmergencyContact(
+  contact_number,
+  name,
+  relationship
+) {
+  const SQL =
+    "INSERT INTO emergency_contact (contact_number, name, relationship) VALUES $1, $2,$3 RETURNING *";
+
+  const {
+    rows: [emergencyContact],
+  } = await db.query(SQL, [contact_number, name, relationship]);
+
+  return emergencyContact;
+}
