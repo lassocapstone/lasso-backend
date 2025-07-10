7266e60347937ee7f157e2b6a23c7f1a44d61191
TanerJackson <jacksontaner@gmail.com>
2025-07-10 11:50:25 -0500

    all tables seeded with fake data

--- db/seed.js
+++ db/seed.js
@@ -1,8 +1,10 @@
 import db from "#db/client";
-import { createUser, getUserById } from "#db/queries/users";
+import { createUser } from "#db/queries/users";
+import { createEvent } from "#db/queries/events";
+import { createAlert } from "#db/queries/alerts";
+import { createTask } from "#db/queries/tasks";
 import { createEmergencyContact } from "#db/queries/emergencycontact";
 import { faker } from "@faker-js/faker";
-
 await db.connect();
 await seed();
 await db.end();
@@ -38,10 +40,53 @@ async function seed() {
       "5551112222"
     );
   }
+  //events have:
+  //name, start_time, end_time, location, organizer_id
+
+  await createEvent(
+    "Museum Visit",
+    "14:00:00",
+    "18:00:00",
+    "The museum of stuff",
+    1
+  );
+
+  await createEvent(
+    "Museum buyout",
+    "15:00:00",
+    "19:00:00",
+    "Things the museum",
+    1
+  );
+
+  //alerts have:
+  //is_okay, name, message, event_id, sender_id
+  await createAlert("true", "stairs hard", "bobby fell down the stairs", 1, 2);
+  await createAlert(
+    "true",
+    "stairs super hard",
+    "jimmy fell down the stairs",
+    2,
+    2
+  );
+  await createAlert("false", "bad stairs", "sally fell down the stairs", 1, 3);
+
+  //tasks have:
+  //event_id, name, start_time, end_time, location, instructions
+  await createTask(
+    1,
+    "Take Attendance Upon arrival",
+    "14:30:00",
+    "14:35:00",
+    "At the museum",
+    "count your group members and report all good on task"
+  );
+  await createTask(
+    2,
+    "Take Attendance Upon arrival",
+    "14:30:00",
+    "14:35:00",
+    "At the museum",
+    "count your group members and report all good on task"
+  );
 }
-
-
-
-
-
-
