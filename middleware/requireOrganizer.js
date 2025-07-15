3b098a3e6f1422bc3046864f8dc3f9fd531b96d9
Bryan Cotto <brycotto1@yahoo.com>
2025-07-15 09:39:58 -0400

    Correct || to && statement in requireOrganizer middleware

--- middleware/requireOrganizer.js
+++ middleware/requireOrganizer.js
@@ -1,4 +1,4 @@
-export default requireOrganizer = (req, res, next) => {
-  if(req.user.accountType !== "org") res.status(403).send("Authorization denied. User not an organizer.");
+export default function requireOrganizer(req, res, next) {
+  if(req.user.account_type !== "org") res.status(403).send("Authorization denied. User not an organizer.");
   next();
 }
