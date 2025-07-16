9b795ea2685b48b876f01fc38b9e604a914d8bee
Bryan Cotto <brycotto1@yahoo.com>
2025-07-15 20:53:38 -0400

    Cleaned up tasks router and tasks db queries

--- api/tasks.js
+++ api/tasks.js
@@ -1,33 +1,52 @@
 import express from "express";
-const tasksRouter = express.Router();
-export default tasksRouter;
+const router = express.Router();
+export default router;
 
-import requireEvent from "#middleware/requireEvent";
 import requireTask from "#middleware/requireTask";
 
-import { getTaskById } from "#db/queries/tasks";
-import { deleteTaskById } from "#db/queries/tasks";
-import { getEventById } from "#db/queries/events";
+import { getTaskById, deleteTaskById, updateTaskById, createTask, getTasksByEvent } from "#db/queries/tasks";
 import requireOrganizer from "#middleware/requireOrganizer";
+import requireBody from "#middleware/requireBody";
 
-tasksRouter.use(requireEvent);
-
-tasksRouter.get("/", async (req, res) => {
-
+router.route("/")
+  .get(
+    async (req, res) => {
+      const tasks = await getTasksByEvent(req.event.id);
+      res.send(tasks);
+})
+  .post(
+    requireOrganizer,
+    requireBody(["name", "startTime", "endTime", "location", "instructions"]),
+    async (req, res) => {
+      const {name, startTime, endTime, location, instructions} = req.body;
+      const task = await createTask(req.event.id, name, startTime, endTime, location, instructions);
+      res.status(201).send(task);
 });
 
-tasksRouter.post("/", requireOrganizer, async (req, res) => {
- 
-});
+router.route("/:taskId")
+  .get(
+    requireTask, 
+    async (req, res) => {
+      const {taskId} = req.params;
+      const task = await getTaskById(taskId);
+      res.send(task);
+})
+  .put(
+    requireTask,
+    requireOrganizer,
+    requireBody(["eventId", "name", "startTime", "endTime", "location", "instructions"]),
+    async (req, res) => {
+      const {taskId} = req.params;
+      const {eventId, name, startTime, endTime, location, instructions} = req.body;
 
-tasksRouter.get("/:taskId", requireTask, async (req, res) => {
-  
-});
-
-tasksRouter.put("/:taskId", requireTask, requireOrganizer, async (req, res) => {
-  
-});
-
-tasksRouter.delete("/:taskId", requireTask, requireOrganizer, async (req, res) => {
-
+      const updatedTask = await updateTaskById(taskId, eventId, name, startTime, endTime, location, instructions);
+      res.send(updatedTask);
+})
+  .delete(
+    requireTask, 
+    requireOrganizer,
+    async (req, res) => {
+      const {taskId} = req.params;
+      await deleteTaskById(taskId);
+      res.status(204).send("Task deleted");
 });
--- middleware/requireTask.js
+++ middleware/requireTask.js
@@ -6,5 +6,6 @@ export default async function requireTask(req, res, next) {
   const task = await getTaskById(taskId);
   if(!task) return res.status(404).send("That task does not exist");
 
+  req.task = task;
   next();
 }
